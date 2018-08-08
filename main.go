f3eb8783b6063b830a4891a1d70c32ae21f20d06
calebblanchard <caleb.blanchard@maine.edu>
2018-08-08 06:49:52 -0400

    Listening and serving on port 8000

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"log"
+	"net/http"
+
 	"github.com/gorilla/mux"
 )
 
@@ -14,4 +17,6 @@ func main() {
 	r.HandleFunc("api/books", createBook).Methods(POST)
 	r.HandleFunc("api/books/{id}", updateBook).Methods(PUT)
 	r.HandleFunc("api/books/{id}", deleteBook).Methods(DELETE)
+
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
