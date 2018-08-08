792f6cf1d87c3f9f7ed9c10164648622fbf91b5b
calebblanchard <caleb.blanchard@maine.edu>
2018-08-08 06:45:56 -0400

    Added router and route handlers/endpoints

--- main.go
+++ main.go
@@ -1,14 +1,17 @@
 package main
 
 import (
-	"encoding/json"
-	"log"
-	"net/http"
-	"math/rand"
-	"strconv"
 	"github.com/gorilla/mux"
 )
 
 func main() {
-	
+	// Init Router
+	r := mux.NewRouter()
+
+	// Route Handlers / Endpoints
+	r.HandleFunc("api/books", getBooks).Methods(GET)
+	r.HandleFunc("api/books/{id}", getBook).Methods(GET)
+	r.HandleFunc("api/books", createBook).Methods(POST)
+	r.HandleFunc("api/books/{id}", updateBook).Methods(PUT)
+	r.HandleFunc("api/books/{id}", deleteBook).Methods(DELETE)
 }
